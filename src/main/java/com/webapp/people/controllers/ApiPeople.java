e5db864d062e0068ef062d26eb595443cd58b97b
Camila Mamani <camila.mamani@ibm.com>
2022-10-07 21:08:32 -0300

    updated apipeople

--- src/main/java/com/webapp/people/controllers/ApiPeople.java
+++ src/main/java/com/webapp/people/controllers/ApiPeople.java
@@ -2,31 +2,36 @@ package com.webapp.people.controllers;
 
 import com.webapp.people.model.Persona;
 import com.webapp.people.service.PersonaService;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("api")
 public class ApiPeople {
     @Autowired
     PersonaService personasService;
 
-    @GetMapping("/saludar")
-    public String saludar(){
-        return "Hola";
-    }
-
     @GetMapping("/personas")
     public ArrayList<Persona> getAllPersonas(){
-
         return personasService.getAllPersonas();
     }
 
-    @PostMapping("/persona/ guardar")
+    @PostMapping("/persona/guardar")
     public Persona savePersona(@RequestBody Persona a){
         return personasService.savePersona(a);
     }
+
+    @GetMapping("/personas/{name}/{surname}")
+    public ArrayList<Persona> filterPersonas(@PathVariable("name") String name, @PathVariable("surname") String surname){
+        return personasService.filterPersonas(name, surname);
+    }
+
 }
