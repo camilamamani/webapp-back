90b32926436f5d473aa405e3900a80fcb0b27985
Camila Mamani <camila.mamani@ibm.com>
2022-10-07 21:05:39 -0300

    Add filterPersona PersonaService

--- src/main/java/com/webapp/people/service/PersonaService.java
+++ src/main/java/com/webapp/people/service/PersonaService.java
@@ -5,4 +5,5 @@ import java.util.ArrayList;
 public interface PersonaService {
     ArrayList<Persona> getAllPersonas();
     Persona savePersona(Persona a);
+    ArrayList<Persona> filterPersonas(String nombre, String apellido);
 }
--- src/main/java/com/webapp/people/service/PersonaServiceImpl.java
+++ src/main/java/com/webapp/people/service/PersonaServiceImpl.java
@@ -6,12 +6,19 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import com.webapp.people.model.Persona;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+
 @Service
 public class PersonaServiceImpl implements PersonaService{
     @Autowired
     PersonaRepository personasRep;
+    @Autowired
+    private EntityManagerFactory emf;
     @Override
     public ArrayList<Persona> getAllPersonas() {
+
         return (ArrayList<Persona>) personasRep.findAll();
     }
 
@@ -19,4 +26,13 @@ public class PersonaServiceImpl implements PersonaService{
     public Persona savePersona(Persona a){
         return personasRep.save(a);
     }
+
+    @Override
+    public ArrayList<Persona> filterPersonas(String nombre, String apellido) {
+        EntityManager em = emf.createEntityManager();
+        Query query = em.createQuery("from Persona where name like '"+nombre+"%' and surname like '"+apellido+"%'");
+        ArrayList<Persona> personas = (ArrayList<Persona>) query.getResultList();
+        return personas;
+    }
+
 }
